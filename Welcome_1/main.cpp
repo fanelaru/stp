5b9bfeade6356fac0284e9ee5f36624668b13b0a
sboros <sboros@STNW-PORT87.stnw.pentalog.fr>
2013-10-24 09:38:47 +0300

    Copiat din GitHub

---
+++ Welcome_1/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: sboros
+ *
+ * Created on October 24, 2013, 9:29 AM
+ */
+
+#include "Interfata.h"
+
+using namespace std;
+
+int main(void)
+{
+    Interfata *interfata = new Interfata;
+    interfata->display(); //Incarca interfata principala - View
+    return 0;
+}
